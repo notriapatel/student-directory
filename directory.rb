bf64665cdc46ba7158cf4992e20e91962cf32cdb
ria patel <ria.amy.patel@gmail.com>
2019-12-05 15:33:59 +0000

    saving data

--- directory.rb
+++ directory.rb
@@ -21,6 +21,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the lsit to students.csv"
   puts "9. Exit"
 end
 
@@ -36,6 +37,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -56,6 +59,18 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  #
+  file = File.open("students.csv", "w")
+  #
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
